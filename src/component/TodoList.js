4c652a8e5a3f19e7c448948d2bd509c8a31a1b6e
Nati Lavega <nati.lavega@gmail.com>
2022-02-11 14:28:17 -0300

    small design changes

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClipboardCheck } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardCheck } from '@fortawesome/free-solid-svg-icons';
 
 const TodoList = ({ todos, deleteTodo }) => {
+  const todoList = todos.length ? (
+    todos.map((todo) => {
+      return (
+        <ul id='todo' className='fadeIn' key={todo.id}>
+          <li
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            {todo.content}
+          </li>
+        </ul>
+      );
+    })
+  ) : (
+    <div id='empty' className='fadeIn'>
+      <div>
+        <FontAwesomeIcon icon={faClipboardCheck} />
+      </div>
+      <p>No hay tareas pendientes.</p>
+    </div>
+  );
 
-    const todoList = todos.length ? (
-        todos.map(todo => {
-            return (
-                <ul id="todo" className="fadeIn" key={todo.id}>
-                    <li onClick={() => { deleteTodo(todo.id) }}>{todo.content}</li>
-                </ul>
-            )
-        })
-    ) : (
-            <div id="empty" className="fadeIn">
-                <p>No hay tareas pendientes.</p>
-                <div>
-                    <FontAwesomeIcon icon={faClipboardCheck} />
-                </div>
-            </div>
-        );
+  return <div>{todoList}</div>;
+};
 
-    return (
-        <div>
-            {todoList}
-        </div>
-    )
-}
-
-export default TodoList
+export default TodoList;
